3d30fe8cae6bbb83ad9a6c2f6be64799307a9ecb
haqnawazmr6-ui <haqnawazmr6@gmail.com>
2026-05-17 23:52:38 +0500

    Add user report command functionality

---
+++ plugins/User-report.js
@@ -0,0 +1,33 @@
+const { cmd } = require('../command');
+
+cmd({
+    pattern: "nawaz2",
+    react: "🚨",
+    category: "misc",
+    filename: __filename
+}, async (conn, mek, m, { from, sender, text }) => {
+
+    const owner = "923161483125@s.whatsapp.net"; // درست JID format
+
+    if (!text) {
+        return conn.sendMessage(from, {
+            text: "❌ Please write something to report\nExample: .report bot is not working"
+        }, { quoted: mek });
+    }
+
+    // user response
+    await conn.sendMessage(from, {
+        text: "✅ Report sent successfully"
+    }, { quoted: mek });
+
+    // owner ko report
+    await conn.sendMessage(owner, {
+        text:
+`🚨 NEW REPORT RECEIVED
+
+👤 From: ${sender}
+📍 Chat: ${from}
+📝 Report: ${text}`
+    }, { quoted: mek });
+
+});
