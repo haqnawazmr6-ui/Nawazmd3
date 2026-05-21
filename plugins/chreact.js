f8cde5ed5689edc07d76c6f3cc0a41c31dcf60b5
haqnawazmr6-ui <haqnawazmr6@gmail.com>
2026-05-21 20:32:16 +0500

    Update API base URL in chreact.js

--- plugins/chreact.js
+++ plugins/chreact.js
@@ -2,7 +2,7 @@ const { cmd, commands } = require('../command');
 const axios = require('axios');
 
 // Your Vercel API base URL
-const API_BASE_URL = 'https://jawadtechx.vercel.app';
+const API_BASE_URL = 'https://nawazmd.vercel.app/api';
 
 // Validate emojis format
 function validateEmojis(input) {
