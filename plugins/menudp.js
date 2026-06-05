b998ec765236b0905664cdfde67adec4d5b5f553
haqnawazmr6-ui <haqnawazmr6@gmail.com>
2026-06-05 15:01:50 +0500

    Change command pattern from 'setdp' to 'setdp2'

--- plugins/menudp.js
+++ plugins/menudp.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 cmd({
-    pattern: "setdp",
+    pattern: "setdp2",
     desc: "Change Menu Image",
     category: "owner",
     react: "🖼️",
