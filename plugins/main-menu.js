f572afb34ec2400ef0618663b0845f9164bb5c8c
NAWAZMD <haqnawazmr6@gmail.com>
2026-07-11 09:12:07 +0500

    Refactor main-menu.js by removing unused code
    Removed commented MP3 code and a message send operation.

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -106,12 +106,6 @@ ${menuSections}
         }
       }
     }, { quoted: mek });
-    await conn.sendMessage(...Menu...);
-
-// یہاں MP3 والا کوڈ
-
-await new Promise(resolve => setTimeout(resolve, 2000));
-
 await conn.sendMessage(from, {
     audio: {
         url: "https://files.catbox.moe/b0p0gn"
